1805fd5261f8d7e2190a492e78a115c934a1496a
Cristina Mantiñán <56038975+CrisMB-82@users.noreply.github.com>
2025-03-16 21:40:29 +0000

    My first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-import "bootstrap";
-import "./style.css";
+window.onload = generarExcusa;
+
+function generarExcusa() {
+  let who =['El perro', 'Mi abuela','El cartero','Mi pájaro'];
+  let action =['se comió', 'orinó en', 'aplastó','rompió'];
+  let what =['mi tarea', 'mi teléfono','el coche'];
+  let when =['antes de la clase', 'cuando estaba durmiendo', 'mientras hacía ejercicio','durante mi almuerzo','mientras estaba rezando'];
+  
+  let ex1 =who[Math.floor(Math.random() * who.length)];
+  let ex2 =action[Math.floor(Math.random() *action.length)];
+  let ex3 = what[Math.floor(Math.random() * what.length)];
+  let ex4 = when[Math.floor(Math.random() *when.length)];
+
+// Genera la excusa combinando las partes
+  let excusa = `${ex1} ${ex2} ${ex3} ${ex4}.`;
+  
+  document.getElementById("excusa").innerText = excusa;
+}
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
